4a2263e956128cafecf55cc2e36b32efbd122d18
Billy K. Poon <bkpoon@lbl.gov>
2022-07-20 10:00:21 -0700

    Run rsync from $BUILD_PREFIX

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,4 +8,8 @@ cd src
 make -f Makefile.ap install
 cd ..
 
-rsync -av bin dat lib $PREFIX
+RSYNC=rsync
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  RSYNC=${BUILD_PREFIX}/bin/rsync
+fi
+${RSYNC} -av bin dat lib $PREFIX
