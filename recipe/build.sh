f0ec181dd991f6fb1deb913049aaa03c618609b1
Billy K. Poon <bkpoon@lbl.gov>
2022-07-20 11:13:19 -0700

    Add osx-64 utilMakeHelp command

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,6 @@ set -xe
 RSYNC=rsync
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
   RSYNC=${BUILD_PREFIX}/bin/rsync
-  CONDA_SUBDIR=osx-64 mamba install -y -p ${BUILD_PREFIX} -c phenix-project amber_phenix
 fi
 
 export MSANDERHOME=`pwd`
