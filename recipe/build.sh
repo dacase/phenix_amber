1c2d5ca6553e7575855719ee376b2ec2fd3438ac
Billy K. Poon <bkpoon@lbl.gov>
2022-07-20 10:10:14 -0700

    Manuall install amber_phenix for cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -xe
 
+# cross compiling options for conda
+RSYNC=rsync
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  RSYNC=${BUILD_PREFIX}/bin/rsync
+  CONDA_SUBDIR=osx-64 mamba install -y -p ${BUILD_PREFIX} -c phenix-project amber_phenix
+fi
+
 export MSANDERHOME=`pwd`
 ./configure --conda --openmp
 
@@ -8,8 +15,4 @@ cd src
 make -f Makefile.ap install
 cd ..
 
-RSYNC=rsync
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
-  RSYNC=${BUILD_PREFIX}/bin/rsync
-fi
 ${RSYNC} -av bin dat lib $PREFIX
