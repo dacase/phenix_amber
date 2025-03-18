acfc3150764248e1ff60e96615ebffd565794477
David A Case <david.case@rutgers.edu>
2025-03-18 15:17:49 -0600

    recipe copied from Billy's github repo

---
+++ recipe.bkpoon/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -xe
+
+# cross compiling options for conda
+RSYNC=rsync
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  RSYNC=${BUILD_PREFIX}/bin/rsync
+fi
+
+export MSANDERHOME=`pwd`
+./configure --conda --openmp
+
+# check configuration
+cat config.h
+
+cd src
+make -f Makefile.ap install
+cd ..
+
+# remove libblas.a and liblapack.a
+rm -f ./lib/libblas.a
+rm -f ./lib/liblapack.a
+
+${RSYNC} -av bin dat lib $PREFIX
