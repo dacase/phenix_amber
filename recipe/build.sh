2c238eb8ba1f410bd471397d17c3d12b276ddafb
Billy K. Poon <bkpoon@lbl.gov>
2025-05-21 11:30:25 -0700

    Add conda-forge feedstock files

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
+set -xe
+
+# cross compiling options for conda
+RSYNC=rsync
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+  RSYNC=${BUILD_PREFIX}/bin/rsync
+fi
 
 export MSANDERHOME=`pwd`
 ./configure --conda --openmp
+
+# check configuration
+cat config.h
 
 cd src
 make -f Makefile.ap install
 cd ..
 
-rsync -av bin dat lib $PREFIX
+${RSYNC} -av bin dat lib $PREFIX
